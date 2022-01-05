9215b3a98ba6cc6ea5e23216da53c71d1e1105e2
Marcel Huber <git@marcelhuber.de>
2022-01-05 10:41:35 +0100

    feat(apiselector): add popup for publishing

--- pages/api-selector.vue
+++ pages/api-selector.vue
@@ -61,18 +61,78 @@
                   <v-card-actions>
                     <v-spacer />
                     <v-btn color="primary" @click="submit(api)">
-                      Submit changes
+                      Save changes
                     </v-btn>
+                    <v-dialog v-model="dialog" width="800">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                          Publish API
+                        </v-btn>
+                      </template>
+
+                      <v-card>
+                        <v-card-title class="text-h5 grey darken-2">
+                          {{ api.title }}: Selected Endpoints
+                        </v-card-title>
+                        <v-list two-line>
+                          <template v-for="endpoint in api.endpoints">
+                            <template v-if="endpoint.checked">
+                              <v-list-item :key="getLabel(endpoint)">
+                                <v-list-item-content>
+                                  <v-row>
+                                    <v-col cols="4">{{
+                                      getLabel(endpoint)
+                                    }}</v-col>
+                                    <v-col
+                                      ><div>
+                                        <strong>Description: </strong>
+                                        <div>{{ endpoint.description }}</div>
+                                      </div>
+                                      <div>
+                                        <div
+                                          v-if="endpoint.parameters.length > 0"
+                                        >
+                                          Parameters:
+                                        </div>
+                                        <ul>
+                                          <li
+                                            v-for="parameter in endpoint.parameters"
+                                            :key="parameter.name"
+                                          >
+                                            <div>
+                                              <strong
+                                                >{{ parameter.name }}:
+                                              </strong>
+                                              {{ parameter.description }}
+                                            </div>
+                                          </li>
+                                        </ul>
+                                      </div></v-col
+                                    >
+                                  </v-row>
+                                </v-list-item-content>
+                              </v-list-item>
+                            </template>
+                          </template>
+                        </v-list>
+
+                        <v-divider></v-divider>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn text @click="dialog = false"> Cancel </v-btn>
+                          <v-btn color="primary" text @click="publish()">
+                            Publish
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
                   </v-card-actions>
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </template>
           </v-expansion-panels>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary"> Confirm Selection </v-btn>
-        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
@@ -133,7 +193,7 @@ export default {
       event.cancelBubble = true
     },
     getLabel(selection) {
-      return selection.method + ' ' + selection.path
+      return selection.method.toUpperCase() + ' ' + selection.path
     },
     submit(api) {
       const requestBody = {
@@ -162,6 +222,9 @@ export default {
         'http://localhost:8082/apis/' + api.id + '/update',
         requestBody
       )
+    },
+    publish() {
+      this.dialog = false
     },
   },
 }
